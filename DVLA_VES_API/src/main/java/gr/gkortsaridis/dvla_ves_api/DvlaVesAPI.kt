41cb1c271ff51a3563e605151365aa042b5df0c7
George Kortsaridis <gkortsaridis@gmail.com>
2020-10-10 10:10:11 +0100

    API Setup

---
+++ DVLA_VES_API/src/main/java/gr/gkortsaridis/dvla_ves_api/DvlaVesAPI.kt
@@ -0,0 +1,11 @@
+package gr.gkortsaridis.dvla_ves_api
+
+object DvlaVesAPI {
+
+    private val TOKEN = "3KqBAOt2I66r5TbLC0ebB8XJdPlW5pvv7brxSnCx"
+
+    fun getVehicleDetails(registrationNumber: String) : String {
+        return "HELLO WORLD"
+    }
+
+}
